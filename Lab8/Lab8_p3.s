fa7963a802cb79115011d33d1606941281cb1eb6
Drini Kerçiku <48163907+DriniKerciku@users.noreply.github.com>
2019-11-24 20:47:13 -0500

    Update and rename Lab8_p3.txt to Lab8_p3.s

--- Lab8/Lab8_p3.s
+++ Lab8/Lab8_p3.s
@@ -1,10 +1,11 @@
+//Count the number of consecutive ones in a word
 .text
 .global start
 start:
 	LDR R1, TEST_NUM // load the data word into R1
 	MOV R0, #0 // R0 will hold the result
 LOOP: 
-	CMP R1, #0 // loop until the data contains no more 1’s
+	CMP R1, #0 // loop until the data contains no more 1â€™s
 	BEQ END
 	LSR R2, R1, #1 // perform SHIFT, followed by AND
 	AND R1, R1, R2
